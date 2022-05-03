ec30a4ef656a8349b67a2b946f23f1bb046ccda1
Tomasz Boroń <tomaszboron2k@gmail.com>
2022-05-03 11:47:23 +0200

    chore: create unit tests for database

--- src/app.py
+++ src/app.py
@@ -9,19 +9,6 @@ from widgets import MainWindow
 
 
 def main():
-    # uncomment below to show database in action
-    # writer = Writer("db", "collection")
-    # for i in range(11):
-    #     writer.add_document({"key": "value", "another_key": 1})
-    # reader = Reader("db", "collection")
-    # print(reader.get_rows_number()) # number of rows example
-    # print(reader.get_columns_names()) # columns headers
-    # print(reader.execute_query(columns=["key", "another_key"])) # show specified columns only
-    # print(reader.get_nth_chunk(columns=["key", "another_key"])) # nth-chunk with chunk_size=0 and chunk_number=0 returns all records in dataframe
-    # print(reader.execute_query(query={"another_key": 1})) # query example
-    # print(reader.get_nth_chunk()) # all records, equivalent to reader.execute_query, but result is in dataframe
-    # print(reader.get_nth_chunk(use_id=1, columns=["key", "another_key"])) # get id
-
     # database reader showup
     # db_reader = DatabaseReader("db", "collection")
     # db_reader._read_all(["key", "another_key"])
---
+++ test/database_tests/test_database_reader.py
@@ -0,0 +1,35 @@
+from unittest import TestCase # if authorization fails, set env variable MONGO_PASS
+import sys
+import os
+import pandas as pd
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../../src'))
+from src.database import Reader, Writer, DocumentRemover
+
+
+class TestReader(TestCase):
+    def setUp(self) -> None:
+        self.reader = Reader("test", "test")
+        self.query_test_result = [
+            {'key': 'value', 'another_key': 1},
+            {'key': 'value', 'another_key': 1},
+            {'key': 'value', 'another_key': 1},
+            {'key': 'value', 'another_key': 1}
+        ]
+        DocumentRemover("test", "test").remove_all()
+        writer = Writer("test", "test")
+        for i in range(4):
+            writer.add_document({"key": "value", "another_key": 1})
+
+    def test_execute_query(self):
+        self.assertEqual(self.reader.execute_query(columns=["key", "another_key"]), self.query_test_result)
+
+    def test_get_nth_chunk(self):
+        df = pd.DataFrame(self.query_test_result)
+        self.assertDictEqual(df.to_dict(), self.reader.get_nth_chunk().to_dict())
+
+    def test_get_rows_number(self):
+        self.assertEqual(self.reader.get_rows_number(), 4)
+
+    def test_get_columns_names(self):
+        self.assertEqual(self.reader.get_columns_names(), ["_id", "key", "another_key"])
