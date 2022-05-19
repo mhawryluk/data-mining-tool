84cd2743cb56cb3241642382d58c5f3b0c396de3
mhawryluk <marcinhawrylukxx@gmail.com>
2022-05-20 00:27:14 +0200

    chore: Start implementing ResultsWidget and ResultsEngine

--- src/widgets/unfold_widgets/results_widget.py
+++ src/widgets/unfold_widgets/results_widget.py
@@ -1,4 +1,6 @@
-from widgets import UnfoldWidget
+from PyQt5.QtWidgets import QTableView, QSizePolicy, QHBoxLayout, QVBoxLayout, QTabWidget, QLabel
+
+from widgets import UnfoldWidget, QtTable
 
 
 class ResultsWidget(UnfoldWidget):
@@ -6,6 +8,31 @@ class ResultsWidget(UnfoldWidget):
         super().__init__(parent, engine, 'results_widget', 'RESULTS')
         self.button.disconnect()
         self.button.clicked.connect(self.load_widget)
+        self.engine = engine
+
+        # data table
+        self.data_table = QTableView(self)
+        # self.data_table.setMinimumHeight(300)
+        self.data_table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        # algorithm results tab widget
+        self.results_tab_widget = QTabWidget(self)
+        self.results_tab_widget.addTab(QLabel("Result"), "Clastering")
+
+        # layouts for sections
+        self.layout = QVBoxLayout(self.frame)
+
+        self.first_row = QHBoxLayout()
+        self.first_row.addWidget(self.results_tab_widget)
+        self.first_row.setSpacing(35)
+
+        self.second_row = QHBoxLayout()
+        self.second_row.addWidget(self.data_table, 0)
+
+        self.layout.addLayout(self.first_row, 3)
+        self.layout.addLayout(self.second_row, 1)
 
     def load_widget(self):
         self.parent().unfold(self)
+        if (data := self.engine.state.imported_data) is not None:
+            self.data_table.setModel(QtTable(data))
