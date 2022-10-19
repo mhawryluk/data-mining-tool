1fb61f201c98eecbe1b587508bd974827f305401
Tomasz Boroń <tomaszboron2k@gmail.com>
2022-10-19 22:39:20 +0200

    Show final mean and variance on results screen

--- src/visualization/clustering/gmm_vis.py
+++ src/visualization/clustering/gmm_vis.py
@@ -30,6 +30,22 @@ class GMMCanvas(FigureCanvasQTAgg):
         if self.animation:
             return self.axes.collections
 
+    def _draw_variance(self, mean, sigma, label, max_label, n_std=2.0):
+        pearson = sigma[0][1] / np.sqrt(sigma[0][0] * sigma[1][1])
+        ell_radius_x = np.sqrt(1 + pearson)
+        ell_radius_y = np.sqrt(1 - pearson)
+        cmap = plt.get_cmap('gist_rainbow')
+        ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                          edgecolor=cmap(label / max_label), facecolor='none')
+        scale_x = np.sqrt(sigma[0][0]) * n_std
+        scale_y = np.sqrt(sigma[1][1]) * n_std
+        transf = transforms.Affine2D() \
+            .rotate_deg(45) \
+            .scale(scale_x, scale_y) \
+            .translate(mean[0], mean[1])
+        ellipse.set_transform(transf + self.axes.transData)
+        self.axes.add_patch(ellipse)
+
     def clusters_plot(self, vector_x, vector_y, columns, mean, sigma, labels, max_label, name_x, name_y, min_x, max_x,
                       min_y, max_y, drawing=True):
         self.axes.cla()
@@ -47,28 +63,42 @@ class GMMCanvas(FigureCanvasQTAgg):
                 [sigma[i][y_index][x_index], sigma[i][y_index][y_index]],
             ]
             self.axes.scatter(mean_i[0], mean_i[1], c=i, cmap='gist_rainbow', marker='s', vmin=0, vmax=max_label)
-
-            # ellipse drawing
-            n_std = 3.0
-            pearson = sigma_i[0][1] / np.sqrt(sigma_i[0][0] * sigma_i[1][1])
-            ell_radius_x = np.sqrt(1 + pearson)
-            ell_radius_y = np.sqrt(1 - pearson)
-            cmap = plt.get_cmap('gist_rainbow')
-            ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
-                              edgecolor=cmap(i/max_label), facecolor='none')
-            scale_x = np.sqrt(sigma_i[0][0]) * n_std
-            scale_y = np.sqrt(sigma_i[1][1]) * n_std
-            transf = transforms.Affine2D() \
-                .rotate_deg(45) \
-                .scale(scale_x, scale_y) \
-                .translate(mean_i[0], mean_i[1])
-            ellipse.set_transform(transf + self.axes.transData)
-            self.axes.add_patch(ellipse)
+            self._draw_variance(mean_i, sigma_i, i, max_label)
 
         if drawing:
             self.draw()
         if self.animation:
             return self.axes.collections
+
+    def chosen_cluster_plot(self, vector_x, vector_y, mean, sigma, label, max_label, name_x, name_y, min_x, max_x,
+                            min_y, max_y, drawing=True):
+        self.axes.cla()
+        self.axes.set_xlabel(name_x)
+        self.axes.set_ylabel(name_y)
+        self.axes.set_xlim(min_x, max_x)
+        self.axes.set_ylim(min_y, max_y)
+        self.axes.scatter(vector_x, vector_y, c=[label] * len(vector_x), cmap='gist_rainbow',
+                          vmin=0, vmax=max_label, alpha=0.9)
+        self.axes.scatter([mean[0]], [mean[1]], c=[label], cmap='gist_rainbow', vmin=0, vmax=max_label,
+                          edgecolor='black', linewidths=1, marker='s', alpha=0.7, s=50)
+        self._draw_variance(mean, sigma, label, max_label, n_std=1.0)
+        if drawing:
+            self.draw()
+
+    def clusters_means_plot(self, means, sigmas, name_x, name_y, min_x, max_x, min_y, max_y, drawing=True):
+        self.axes.cla()
+        x_means, y_means = means
+        max_label = len(x_means)
+        self.axes.set_xlabel(name_x)
+        self.axes.set_ylabel(name_y)
+        self.axes.set_xlim(min_x, max_x)
+        self.axes.set_ylim(min_y, max_y)
+        self.axes.scatter(x_means, y_means, c=np.arange(max_label),
+                          marker='s', cmap='gist_rainbow', vmin=0, vmax=max_label, edgecolor='black', linewidths=1)
+        for i in range(len(x_means)):
+            self._draw_variance([x_means[i], y_means[i]], sigmas[i], i, max_label, n_std=1.0)
+        if drawing:
+            self.draw()
 
 
 class GMMStepsVisualization(QWidget):
--- src/widgets/results_widgets/gmm_results.py
+++ src/widgets/results_widgets/gmm_results.py
@@ -1,12 +1,16 @@
 from functools import partial
 
 import numpy as np
+import pandas as pd
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QGroupBox, QFormLayout, QLabel, QVBoxLayout, QSpinBox, QPushButton, \
-    QComboBox
+    QComboBox, QTableView, QInputDialog, QMessageBox
 from algorithms import get_samples
 from matplotlib import pyplot as plt
 
 from visualization.clustering import GMMCanvas
+
+from src.widgets import QtTable
 
 
 class GMMResultsWidget(QWidget):
@@ -16,7 +20,7 @@ class GMMResultsWidget(QWidget):
         self.columns = self.df.columns
         self.labels = labels
         self.max_label = np.amax(self.labels) + 1
-        self.mean = mean
+        self.mean = pd.DataFrame(mean, columns=self.columns)
         self.sigma = sigma
         self.selected_cluster = None
         self.layout = QHBoxLayout(self)
@@ -71,8 +75,33 @@ class GMMResultsWidget(QWidget):
         self.clusters_canvas = GMMCanvas(self.fig, axes, False)
         self.clustering_group_layout.addWidget(self.clusters_canvas, 1)
 
+        # cluster details
+        self.clusters_details_group = QGroupBox()
+        self.clusters_group_layout = QVBoxLayout(self.clusters_details_group)
+        self.clusters_details_group.setTitle("Cluster")
+        self.clusters_table = QTableView()
+        self.clusters_table.setModel(QtTable(self.mean.round(3)))
+        self.clusters_table.doubleClicked.connect(self.show_cluster)
+        for i in range(len(self.columns)):
+            self.clusters_table.setColumnWidth(i, 120)
+        self.clusters_table_header = QWidget()
+        self.clusters_table_header_layout = QHBoxLayout()
+        self.clusters_table_instruction = QLabel("Double click on any field to preview a cluster")
+        self.save_all_button = QPushButton("SAVE RESULTS")
+        self.save_all_button.clicked.connect(partial(self.on_save_button_click, self.df.assign(cluster=self.labels)))
+        self.save_all_button.setFixedWidth(120)
+        self.clusters_table_header_layout.addWidget(self.clusters_table_instruction)
+        self.clusters_table_header_layout.addWidget(self.save_all_button)
+        self.clusters_table_header.setLayout(self.clusters_table_header_layout)
+        self.fig_distributions, ax = plt.subplots(1, 1)
+        self.clusters_canvas = GMMCanvas(self.fig_distributions, ax, False)
+        self.clusters_group_layout.addWidget(self.clusters_table_header)
+        self.clusters_group_layout.addWidget(self.clusters_table, 1)
+        self.clusters_group_layout.addWidget(self.clusters_canvas, 1)
         self.layout.addWidget(self.clustering_result_group, 1)
+        self.layout.addWidget(self.clusters_details_group, 1)
         self.update_plot()
+        self.update_cluster_plot()
 
     def click_listener(self, button_type: str):
         match button_type:
@@ -81,10 +110,12 @@ class GMMResultsWidget(QWidget):
                 self.num_samples = num
                 self.samples = get_samples(self.df, self.num_samples)
                 self.update_plot()
+                self.update_cluster_plot()
             case 'set_axis':
                 self.ox = self.ox_box.currentText()
                 self.oy = self.oy_box.currentText()
                 self.update_plot()
+                self.update_cluster_plot()
 
     def update_plot(self):
         samples_data = self.df.iloc[self.samples]
@@ -98,6 +129,111 @@ class GMMResultsWidget(QWidget):
         sep_y = 0.1 * (max_y - min_y)
 
         labels = [self.labels[sample] for sample in self.samples]
+        self.clusters_canvas.clusters_plot(x, y, list(self.columns), self.mean.values, self.sigma, labels, self.max_label,
+                                           self.ox, self.oy, min_x - sep_x, max_x + sep_x, min_y - sep_y, max_y + sep_y)
 
-        self.clusters_canvas.clusters_plot(x, y, list(self.columns), self.mean, self.sigma, labels, self.max_label, self.ox,
-                                           self.oy, min_x - sep_x, max_x + sep_x, min_y - sep_y, max_y + sep_y)
+    def update_cluster_plot(self):
+        if self.selected_cluster is not None:
+            indexes = [i for i in range(len(self.labels)) if self.labels[i] == self.selected_cluster]
+            x = self.df.iloc[indexes][self.ox]
+            y = self.df.iloc[indexes][self.oy]
+            min_x = x.min()
+            max_x = x.max()
+            min_y = y.min()
+            max_y = y.max()
+            sep_x = 0.1 * (max_x - min_x)
+            sep_y = 0.1 * (max_y - min_y)
+            x_means = self.mean[self.ox]
+            y_means = self.mean[self.oy]
+            x_index, y_index = [self.columns.get_loc(self.ox), self.columns.get_loc(self.oy)]
+            mean = [x_means.iloc[self.selected_cluster], y_means.iloc[self.selected_cluster]]
+            sigma_helper = self.sigma[self.selected_cluster]
+            sigma = [
+                [sigma_helper[x_index][x_index], sigma_helper[x_index][y_index]],
+                [sigma_helper[y_index][x_index], sigma_helper[y_index][y_index]],
+            ]
+
+            self.clusters_canvas.chosen_cluster_plot(x, y, mean, sigma, self.selected_cluster,
+                                                     len(x_means), self.ox, self.oy, min_x - sep_x, max_x + sep_x,
+                                                     min_y - sep_y, max_y + sep_y)
+        else:
+            x = self.df[self.ox]
+            y = self.df[self.oy]
+            min_x = x.min()
+            max_x = x.max()
+            min_y = y.min()
+            max_y = y.max()
+            sep_x = 0.1 * (max_x - min_x)
+            sep_y = 0.1 * (max_y - min_y)
+            x_means = self.mean[self.ox]
+            y_means = self.mean[self.oy]
+            x_index, y_index = [self.columns.get_loc(self.ox), self.columns.get_loc(self.oy)]
+            means = [x_means, y_means]
+            sigmas = []
+            for i in range(len(self.sigma)):
+                sigma_helper = self.sigma[i]
+                sigmas.append([
+                    [sigma_helper[x_index][x_index], sigma_helper[x_index][y_index]],
+                    [sigma_helper[y_index][x_index], sigma_helper[y_index][y_index]],
+                ])
+
+            self.clusters_canvas.clusters_means_plot(means, sigmas, self.ox, self.oy,
+                                                     min_x - sep_x, max_x + sep_x, min_y - sep_y, max_y + sep_y)
+
+    def show_cluster(self):
+        self.selected_cluster = self.clusters_table.selectionModel().selectedIndexes()[0].row()
+        rows = [i for i in range(len(self.labels)) if self.labels[i] == self.selected_cluster]
+        elements = self.df.iloc[rows]
+        self.clusters_table.setModel(QtTable(elements))
+        buttons_widget = QWidget()
+        buttons_layout = QHBoxLayout()
+        exit_button = QPushButton("X")
+        exit_button.clicked.connect(self.exit_from_cluster)
+        exit_button.setFixedWidth(50)
+        save_button = QPushButton("SAVE")
+        save_button.clicked.connect(partial(self.on_save_button_click, elements))
+        save_button.setFixedWidth(100)
+        buttons_layout.addWidget(save_button)
+        buttons_layout.addWidget(exit_button)
+        buttons_layout.setAlignment(Qt.AlignRight)
+        buttons_widget.setLayout(buttons_layout)
+        self.clusters_group_layout.insertWidget(0, buttons_widget)
+        self.clusters_table_header.hide()
+        self.clusters_table.doubleClicked.disconnect()
+        self.update_cluster_plot()
+
+    def exit_from_cluster(self):
+        self.clusters_table.setModel(QtTable(self.mean.round(3)))
+        self.clusters_group_layout.itemAt(0).widget().setParent(None)
+        self.clusters_table_header.show()
+        self.clusters_table.doubleClicked.connect(self.show_cluster)
+        self.selected_cluster = None
+        self.update_cluster_plot()
+
+    def on_save_button_click(self, elements):
+        path, is_ok = QInputDialog.getText(self, 'Save to file', 'Enter filename')
+        if is_ok and path:
+            if not path.endswith(".csv"):
+                path += ".csv"
+            try:
+                elements.to_csv(path)
+            except:
+                error = QMessageBox()
+                error.setIcon(QMessageBox.Critical)
+                error.setText("Something wrong happened while writing data to file. Try again")
+                error.setWindowTitle("Saving failed")
+                error.exec_()
+        elif not is_ok:
+            pass
+        elif not path:
+            error = QMessageBox()
+            error.setIcon(QMessageBox.Critical)
+            error.setText("No path was provided")
+            error.setWindowTitle("Empty path")
+            error.exec_()
+        else:
+            error = QMessageBox()
+            error.setIcon(QMessageBox.Critical)
+            error.setText("This file extension is not supported.")
+            error.setWindowTitle("Unsupported extension")
+            error.exec_()
