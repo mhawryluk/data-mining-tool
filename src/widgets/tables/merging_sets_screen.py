8be60fe2d6eae661b5cb94a332689863215e9abb
Tomasz Boroń <tomaszboron2k@gmail.com>
2022-10-31 15:33:51 +0100

    refactor: Make methods private

--- src/widgets/tables/merging_sets_screen.py
+++ src/widgets/tables/merging_sets_screen.py
@@ -14,7 +14,7 @@ class MergingSetsScreen(QWidget):
         self.engine = self.parent.engine
         self.new_data = None
         self.setWindowTitle("Datasets concatenation")
-        self.load_styles()
+        self._load_styles()
 
         # init all components to have a ref
         self.layout = QHBoxLayout()
@@ -34,17 +34,17 @@ class MergingSetsScreen(QWidget):
         self.columns_merge_group_layout = QHBoxLayout(self.columns_merge_group)
 
         # rendering content
-        self.render_table(self.current_data_view, True)
-        self.render_panel()
-        self.render_table(self.new_data_view, False)
+        self._render_table(self.current_data_view, True)
+        self._render_panel()
+        self._render_table(self.new_data_view, False)
 
         self.setLayout(self.layout)
 
-    def load_styles(self):
+    def _load_styles(self):
         with open('../static/css/styles.css') as stylesheet:
             self.setStyleSheet(stylesheet.read())
 
-    def render_table(self, table_widget, current):
+    def _render_table(self, table_widget, current):
         table_group = QGroupBox(self)
         table_group_layout = QVBoxLayout(table_group)
         table_group.setTitle("Original table" if current else "Table to merge")
@@ -56,14 +56,14 @@ class MergingSetsScreen(QWidget):
         table_group_layout.addWidget(table_widget)
         self.layout.addWidget(table_group, 1)
 
-    def render_panel(self):
-        self.render_import_view()
-        self.render_columns_view()
+    def _render_panel(self):
+        self._render_import_view()
+        self._render_columns_view()
         self.panel.addWidget(self.load_data_group)
         self.panel.addWidget(self.columns_merge_group, 1)
         self.layout.addLayout(self.panel, 1)
 
-    def render_import_view(self):
+    def _render_import_view(self):
         self.load_data_group_layout.setFieldGrowthPolicy(QFormLayout.AllNonFixedFieldsGrow)
         self.load_data_group.setTitle("Load data")
 
@@ -74,7 +74,7 @@ class MergingSetsScreen(QWidget):
         self.filepath_line.setFixedWidth(150)
 
         self.file_button.setText("Select file")
-        self.file_button.clicked.connect(partial(self.click_listener, 'load_file'))
+        self.file_button.clicked.connect(partial(self._click_listener, 'load_file'))
         self.load_data_group_layout.addRow(self.filepath_line, self.file_button)
 
         self.database_label.setText("Choose data from database:")
@@ -85,12 +85,12 @@ class MergingSetsScreen(QWidget):
             self.database_box.addItem(name)
 
         self.database_button.setText("Load")
-        self.database_button.clicked.connect(partial(self.click_listener, 'load_database'))
+        self.database_button.clicked.connect(partial(self._click_listener, 'load_database'))
         self.load_data_group_layout.addRow(self.database_box, self.database_button)
 
         self.load_data_group_layout.addRow(self.import_state_label)
 
-    def render_columns_view(self):
+    def _render_columns_view(self):
         self.columns_merge_group.setTitle("Merge columns")
         instruction = QLabel()
         instruction.setText("Firstly, you need to import another dataset here.\nIf everything goes well, the new "
@@ -101,16 +101,16 @@ class MergingSetsScreen(QWidget):
                             "concatenate, the newly created dataset should be visible on the import screen.")
         self.columns_merge_group_layout.addWidget(instruction, alignment=Qt.AlignTop)
 
-    def click_listener(self, button_type: str):
+    def _click_listener(self, button_type: str):
         match button_type:
             case 'load_file':
-                loading = LoadingWidget(self.load_from_file_handle)
+                loading = LoadingWidget(self._load_from_file_handle)
                 loading.execute()
             case 'load_database':
-                loading = LoadingWidget(self.load_from_database_handle)
+                loading = LoadingWidget(self._load_from_database_handle)
                 loading.execute()
 
-    def load_from_file_handle(self):
+    def _load_from_file_handle(self):
         self.import_state_label.setText("Loading ...")
         file_path: str = QFileDialog.getOpenFileName(self, 'Choose file', '.', "*.csv *.json")[0]
         try:
@@ -119,18 +119,18 @@ class MergingSetsScreen(QWidget):
             self.import_state_label.setText(str(e))
         else:
             self.filepath_line.setText(file_path)
-            self.handle_success()
+            self._handle_success()
 
-    def load_from_database_handle(self):
+    def _load_from_database_handle(self):
         self.import_state_label.setText("Loading ...")
         document_name = self.database_box.currentText()
         error = self.parent.engine.load_data_from_database(document_name)
         if error:
             self.import_state_label.setText(error)
             return
-        self.handle_success()
+        self._handle_success()
 
-    def handle_success(self):
+    def _handle_success(self):
         self.import_state_label.clear()
         if self.parent.engine.is_data_big():
             error = QMessageBox()
@@ -142,7 +142,7 @@ class MergingSetsScreen(QWidget):
         last_preview = self.layout.takeAt(2).widget()
         if last_preview is not None:
             last_preview.deleteLater()
-        self.render_table(self.new_data_view, False)
+        self._render_table(self.new_data_view, False)
 
     def closeEvent(self, event):
         close = QMessageBox.question(self, "QUIT", "Are you sure want to exit process? All changes will be discarded.",
